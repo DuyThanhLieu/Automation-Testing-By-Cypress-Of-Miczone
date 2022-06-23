242b15309dd6fc94c304d2bc1497d1ee23ff5418
DuyThanhLieu <duythanhlieu2001@gmail.com>
2022-06-24 02:34:14 +0700

    update

--- cypress/e2e/1-RatracoSolutions/Login_Ratraco/login.cy.js
+++ cypress/e2e/1-RatracoSolutions/Login_Ratraco/login.cy.js
@@ -2,11 +2,11 @@
  describe('Demo test login ', () => {  
     it ('Verify that user can login with valid username and password', () => {
         cy.visit('https://staging-internal.woka.io/choose-apps')
-        cy.get('#loginEmailInput').type('duythanhlieu2001@gmail.com');
+        cy.get('#loginEmailInput').type('toannguyen@ratracosolutions.com');
         cy.get('#loginPasswordInput').type('123456'); //delay 3s
         //delay 5s for waiting loading
        cy.get('#loginSubmit').click({ delay :5000});
        cy.get('.woka-chooseapps__app-logo').should('be.visible').click();
-
+        cy.get('[data-testid="MenuIcon"] > path').click();
     });
 });
---
+++ cypress/e2e/1-RatracoSolutions/Operate/Operate.cy.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress" />
+ describe('Demo test login ', () => {  
+    it ('Verify that user can login with valid username and password', () => {
+        cy.visit('https://staging-internal.woka.io/choose-apps')
+        cy.get('#loginEmailInput').type('toannguyen@ratracosolutions.com');
+        cy.get('#loginPasswordInput').type('123456'); //delay 3s
+        //delay 5s for waiting loading
+       cy.get('#loginSubmit').click({ delay :5000});
+       cy.get('.woka-chooseapps__app-logo').should('be.visible').click();
+        cy.get('[data-testid="MenuIcon"] > path').click();
+        cy.get('.MuiDrawer-modal > .MuiPaper-root > .box-1 > .css-1hdfbzc > .MuiList-root > :nth-child(6) > .MuiListItemButton-root > .MuiListItemText-root > .MuiTypography-root').click();
+    cy.get('.MuiCollapse-wrapperInner > .MuiList-root > :nth-child(1) > .MuiListItemText-root > .MuiTypography-root').click();
+    });
+});
