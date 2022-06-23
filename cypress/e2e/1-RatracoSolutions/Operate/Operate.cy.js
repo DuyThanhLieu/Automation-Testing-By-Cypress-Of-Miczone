4b39eab9d19fa0e86ccdd465aa996d02441b01f6
DuyThanhLieu <duythanhlieu2001@gmail.com>
2022-06-24 02:49:34 +0700

    updatea

--- cypress/e2e/1-RatracoSolutions/Operate/Operate.cy.js
+++ cypress/e2e/1-RatracoSolutions/Operate/Operate.cy.js
@@ -9,6 +9,11 @@
        cy.get('.woka-chooseapps__app-logo').should('be.visible').click();
         cy.get('[data-testid="MenuIcon"] > path').click();
         cy.get('.MuiDrawer-modal > .MuiPaper-root > .box-1 > .css-1hdfbzc > .MuiList-root > :nth-child(6) > .MuiListItemButton-root > .MuiListItemText-root > .MuiTypography-root').click();
-    cy.get('.MuiCollapse-wrapperInner > .MuiList-root > :nth-child(1) > .MuiListItemText-root > .MuiTypography-root').click();
+   cy.get('.MuiCollapse-wrapperInner > .MuiList-root > :nth-child(1) > .MuiListItemText-root > .MuiTypography-root').click();
+   cy.get('.MuiCollapse-wrapperInner > .MuiList-root > :nth-child(1) > .MuiListItemText-root > .MuiTypography-root').click('bottom');
+    //cy.get('.Mui-selected').click();
+    //cy.get('.css-1coeexk').click('bottom')
+    cy.get('.MuiCollapse-wrapperInner > .MuiList-root > :nth-child(1) > .MuiListItemText-root > .MuiTypography-root').click(80, 75)
+
     });
 });
